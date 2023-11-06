a3043235dd9e847bc9c10eaa0eb14125696c263b
Gubeidulin Timur <timur3009@yandex.ru>
2023-11-06 14:20:28 +0300

    auth page design and toggler finished

--- src/app/(common)/Input.tsx
+++ src/app/(common)/Input.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 
-const Input = () => {
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
     <div className="relative">
       <input
-      id="email"
-        className=" block rounded-md focus:outline-none px-2 pt-4 pb-1 bg-neutral-500 w-full text-white"
+        id={id}
+        value={value}
+        type={type}
+        onChange={onChange}
+        className=" block rounded-md focus:outline-none px-3 pt-5 pb-1 bg-neutral-500 w-full text-white peer"
         placeholder=" "
       />
-      <label htmlFor="email"></label>
+      <label
+        htmlFor={id}
+        className=" absolute text-neutral-400 duration-150 transform -translate-y-3 scale-75 top-3 z-10 origin-[0] left-3 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
+      >
+        {label}
+      </label>
     </div>
   );
 };
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import logo from "../../../public/images/logo.svg";
 import Input from "../(common)/Input";
 
 const Auth = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [status, setStatus] = useState("authorized");
+
+  const toggleStatus = useCallback(() => {
+    setStatus((currentStatus) =>
+      currentStatus === "authorized" ? "unauthorized" : "authorized"
+    );
+  }, []);
+
   return (
     <div className=" relative h-full w-full bg-[url(/images/startingPic.png)] bg-center bg-no-repeat bg-fixed bg-cover">
       <div className=" bg-black w-full h-full lg:bg-opacity-50">
@@ -11,12 +25,51 @@ const Auth = () => {
           <Image src={logo} alt="логотип" width={30} height={30} />
           <p>Словоуч</p>
         </nav>
-        <div className=" flex justify-center">
-          <div className=" bg-black lg:w-2/5 px-10 py-10 bg-opacity-70 rounded-md">
-            <h2 className="text-white font-semibold">Войти в аккаунт</h2>
-            <div className=" flex flex-col gap-4">
-              <Input />
-            </div>
+        <div className=" flex justify-center items-center">
+          <div className=" bg-black lg:w-1/4 px-10 py-10 bg-opacity-70 rounded-md">
+            <h2 className="text-white font-semibold text-xl">
+              {status === "authorized" ? "Залогиниться" : "Зарегистрироваться"}
+            </h2>
+            <form className=" flex flex-col gap-4 mt-3">
+              {status === "unauthorized" && (
+                <Input
+                  label="Имя пользователя"
+                  onChange={(evt: any) => setName(evt.target.value)}
+                  id="user"
+                  value={name}
+                />
+              )}
+              <Input
+                label="Email"
+                onChange={(evt: any) => setEmail(evt.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Пароль"
+                onChange={(evt: any) => setPassword(evt.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </form>
+            <button className=" bg-red-400 rounded-md p-2 w-full mt-8 transition hover:bg-red-500">
+              {status === "unauthorized" ? "Создать аккаунт" : "Войти"}
+            </button>
+            <p className=" text-neutral-400 mt-9 text-center">
+              {status === "authorized"
+                ? "Ещё не регистрировался?"
+                : "Уже есть аккаунт?"}
+              <span
+                onClick={toggleStatus}
+                className=" text-white ml-1 hover:underline-offset-1 cursor-pointer"
+              >
+                {status === "authorized"
+                  ? "Тогда создавай аккаунт!"
+                  : "Тогда залогинься!"}
+              </span>
+            </p>
           </div>
         </div>
       </div>
